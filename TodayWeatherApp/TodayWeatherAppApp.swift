4b440fb594a06e4c2eb1dd8c553152dca04b8ad7
Fuad Fadlila Surenggana <35410577+fuadfs@users.noreply.github.com>
2024-04-30 12:50:59 +0800

    Initial Commit

---
+++ TodayWeatherApp/TodayWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayWeatherAppApp.swift
+//  TodayWeatherApp
+//
+//  Created by Fuad Fadlila Surenggana on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TodayWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
